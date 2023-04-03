f0beaa7b9d72a043b6df10bbc9d6d2bd91db8d88
devleandrodias <leandrodbdias@gmail.com>
2023-04-03 16:13:49 -0300

    feat: finished styling modal input and buttons and add radix-group

--- src/pages/Transactions/components/TransactionModal/index.tsx
+++ src/pages/Transactions/components/TransactionModal/index.tsx
@@ -1,18 +1,40 @@
 import * as Dialog from "@radix-ui/react-dialog";
 
-import { X } from "phosphor-react";
+import { X, ArrowCircleUp, ArrowCircleDown } from "phosphor-react";
 
-import { CloseButton, Content, Overlay } from "./styles";
+import {
+  Content,
+  Overlay,
+  CloseButton,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 
 export function TransactionModal() {
   return (
     <Dialog.Portal>
       <Overlay />
       <Content>
-        <Dialog.Title>Modal de transacao</Dialog.Title>
+        <Dialog.Title>Nova transação</Dialog.Title>
+
         <CloseButton>
           <X size={20} />
         </CloseButton>
+
+        <form>
+          <input required type="number" placeholder="Valor" />
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Depósito
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saque
+            </TransactionTypeButton>
+          </TransactionType>
+          <button type="submit">Confirmar</button>
+        </form>
       </Content>
     </Dialog.Portal>
   );
--- src/pages/Transactions/components/TransactionModal/styles.ts
+++ src/pages/Transactions/components/TransactionModal/styles.ts
@@ -1,6 +1,7 @@
-import * as Dialog from "@radix-ui/react-dialog";
+import styled, { css } from "styled-components";
 
-import styled from "styled-components";
+import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 
 export const Overlay = styled(Dialog.Overlay)`
   inset: 0;
@@ -20,6 +21,47 @@ export const Content = styled(Dialog.Content)`
   left: 50%;
   position: fixed;
   transform: translate(-50%, -50%);
+
+  form {
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    input {
+      border-radius: 6px;
+      border: 0;
+      background: ${(props) => props.theme["gray-900"]};
+      color: ${(props) => props.theme["gray-300"]};
+      padding: 1rem;
+
+      &::placeholder {
+        color: ${(props) => props.theme["gray-500"]};
+      }
+    }
+
+    button[type="submit"] {
+      height: 58px;
+      border: 0;
+      background: ${(props) => props.theme["red-santander-100"]};
+      color: ${(props) => props.theme["white"]};
+      font-weight: bold;
+      padding: 0 1.25rem;
+      border-radius: 6px;
+      margin-top: 0.5rem;
+      cursor: pointer;
+
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+
+      &:hover {
+        background: ${(props) => props.theme["red-santander-200"]};
+        transition: background-color 0.2s;
+      }
+    }
+  }
 `;
 
 export const CloseButton = styled(Dialog.Close)`
@@ -31,4 +73,63 @@ export const CloseButton = styled(Dialog.Close)`
   cursor: pointer;
   line-height: 0;
   color: ${(props) => props.theme["gray-300"]};
+`;
+
+interface TransactionTypeButtonProps {
+  variant: "income" | "outcome";
+}
+
+export const TransactionTypeButton = styled(
+  RadioGroup.Item
+)<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme["gray-700"]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 1rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${(props) => props.theme["gray-300"]};
+
+  svg {
+    color: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-300"]
+        : props.theme["red-300"]};
+  }
+
+  &[data-state="unchecked"]:hover {
+    transition: background-color 0.2s;
+    background: ${(props) => props.theme["gray-600"]};
+  }
+
+  &[data-state="checked"] {
+    color: ${(props) => props.theme["white"]};
+    background: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-500"]
+        : props.theme["red-500"]};
+
+    svg {
+      color: ${(props) => props.theme["white"]};
+    }
+  }
+
+  ${(props) =>
+    props.variant === "outcome" &&
+    css`
+      :focus {
+        outline: 0;
+        box-shadow: 0 0 0 1px ${(props) => props.theme["red-500"]};
+      }
+    `}
+`;
+
+export const TransactionType = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
 `;
