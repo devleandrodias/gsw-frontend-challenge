9af318e59f1794a0d29874bf74c3ef8865c2081e
devleandrodias <leandrodbdias@gmail.com>
2023-04-03 13:06:54 -0300

    feat: installed styled-components and change global styles to use css-js

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,18 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "../styles/global";
+import { defaultTheme } from "../styles/theme/default";
+
 import { Transactions } from "./Transactions";
 import { TransactionProvider } from "../contexts/TransactionContext";
 
 export function App() {
   return (
-    <TransactionProvider>
-      <Transactions />
-    </TransactionProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <TransactionProvider>
+        <Transactions />
+      </TransactionProvider>
+    </ThemeProvider>
   );
 }
